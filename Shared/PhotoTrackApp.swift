1e0f3b34070c03392dd89d6572c750c4b25a6ebd
Christopher Alford <chrisvalford@me.com>
2020-12-15 09:48:11 +0100

    Initial Commit

---
+++ Shared/PhotoTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTrackApp.swift
+//  Shared
+//
+//  Created by Christopher Alford on 15.12.20.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
